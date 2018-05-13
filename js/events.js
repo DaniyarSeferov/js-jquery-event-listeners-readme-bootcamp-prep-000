667ce5ffb2a1ddbcd613afdd95e226fb0f20cfec
Daniyar Seferov <seferovdaniyar@gmail.com>
2018-05-13 17:20:30 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -3,5 +3,15 @@
 $(document).ready(function(){
 
 // call functions here
+function getIt () {
+  $('p').on('click', function() {
+    alert('Hey!');
+  });
+}
 
+function frameIt () {
+  $('img').on('load', function() {
+    $(this).addClass('load');
+  });
+}
 });
